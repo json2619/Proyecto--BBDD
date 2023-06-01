18aaef5f297a585cd86a69aacc53d57519d22ba9
josem <josem@DESKTOP-ASPDV2G>
2023-06-01 22:46:30 +0200

    version 1

--- json/src/main/java/apuesta/Apuesta.java
+++ json/src/main/java/apuesta/Apuesta.java
@@ -1,5 +1,51 @@
 package apuesta;
 
+import com.Gambling.json.TipoJuego;
+
 public class Apuesta {
+
+	private int id;
+	private TipoJuego tipoApuesta;
+	private String fechApuesta;
+	private double precioApuesta;
+
+	public Apuesta(int id, TipoJuego tipoApuesta, String fechApuesta, double precioApuesta) {
+		this.id = id;
+		this.tipoApuesta = tipoApuesta;
+		this.fechApuesta = fechApuesta;
+		this.precioApuesta = precioApuesta;
+	}
+
+	public Apuesta(TipoJuego tipoApuesta, String fechApuesta, double precioApuesta) {
+		this.tipoApuesta = tipoApuesta;
+		this.fechApuesta = fechApuesta;
+		this.precioApuesta = precioApuesta;
+	}
+
+	public double getPrecioApuesta() {
+		return precioApuesta;
+	}
+
+	public void setPrecioApuesta(double precioApuesta) {
+		this.precioApuesta = precioApuesta;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public TipoJuego getTipoApuesta() {
+		return tipoApuesta;
+	}
+
+	public String getFechApuesta() {
+		return fechApuesta;
+	}
+
+	@Override
+	public String toString() {
+		return "Apuesta [id=" + id + ", tipoApuesta=" + tipoApuesta + ", fechApuesta=" + fechApuesta
+				+ ", precioApuesta=" + precioApuesta + "]";
+	}
 
 }
