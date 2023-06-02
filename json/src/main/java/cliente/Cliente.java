93d956ac89aa64ebfe60e7e321a200c29e555aa6
josem <josem@DESKTOP-ASPDV2G>
2023-06-02 11:19:34 +0200

    version 3

--- json/src/main/java/cliente/Cliente.java
+++ json/src/main/java/cliente/Cliente.java
@@ -4,20 +4,11 @@ import java.util.Scanner;
 
 public class Cliente {
 
-	private int id;
 	private String correo;
 	private String contraseña;
 	private String dni;
 	private String telefono;
 	private double saldo;
-
-	public Cliente(int id, String correo, String contraseña, String dni, String telefono) {
-		this.id = id;
-		this.correo = correo;
-		this.contraseña = contraseña;
-		this.dni = dni;
-		this.telefono = telefono;
-	}
 
 	public Cliente(String correo, String contraseña, String dni, String telefono) {
 		this.correo = correo;
@@ -68,18 +59,14 @@ public class Cliente {
 		this.saldo = saldo;
 	}
 
-	public int getId() {
-		return id;
-	}
-
 	public String getDni() {
 		return dni;
 	}
 
 	@Override
 	public String toString() {
-		return "Cliente [id=" + id + ", correo=" + correo + ", contraseña=" + contraseña + ", dni=" + dni
-				+ ", telefono=" + telefono + ", saldo=" + saldo + "]";
+		return "Cliente [correo=" + correo + ", contraseña=" + contraseña + ", dni=" + dni + ", telefono=" + telefono
+				+ ", saldo=" + saldo + "]";
 	}
 
 }
--- json/src/main/java/cliente/Registrador.java
+++ json/src/main/java/cliente/Registrador.java
@@ -1,8 +1,36 @@
 package cliente;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Registrador {
 
-	public static void main(String[] args) {
+	public void insertrSocio(Connection connection, Cliente cliente) throws SQLException {
+		PreparedStatement sentencia = null;
+		ResultSet generatedKeys = null;
 		
+		try {
+			String sql = "INSERT INTO JUGADOR (CORREO, CONTRASEÑA, DNI, TELEFONO) VALUES (?, ? , ?, ?, ?)";
+			sentencia = connection.prepareStatement(sql, java.sql.Statement.RETURN_GENERATED_KEYS);
+			sentencia.setString(1, cliente.getCorreo());
+			sentencia.setString(2, cliente.getContraseña());
+			sentencia.setString(3, cliente.getDni());
+			sentencia.setString(4, cliente.getTelefono());
+			
+			sentencia.executeUpdate();
+			
+			connection.commit();
+		} catch (SQLException sqle) {
+			connection.rollback();
+			
+			sqle.getStackTrace();
+			throw sqle;
+		} finally {
+			if (sentencia != null) {
+				sentencia.close();
+			}
+		}
 	}
 }
